82a19004d3224244856cdfd08f5fc61fbef9771e
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-01-15 16:33:40 +0000

    Hello World

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Everybody." << endl;
+    cout << "This seems to require memorization of Code strings, at least on a glance";
+}
