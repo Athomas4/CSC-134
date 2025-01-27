a8f507c2a18a16a9ed82204f95d0f071685100c0
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-01-27 16:59:31 +0000

    Module 1 Lab 1 Complete 2

--- Module1/m1lab1_Thomas.cpp
+++ Module1/m1lab1_Thomas.cpp
@@ -8,13 +8,15 @@ using namespace std;
 
 int main() {
     string name = "Thomas";
-    int num_apples = 100; //int means whole numbers such as 1, 2, 12353, etc.
-    double cost_each = 0.25; //twenty five cents, .25$
+    int num_apples = 101; //int means whole numbers such as 1, 2, 12353, etc.
+    double cost_each = 1.25; //Double relates to decimal values, such as twenty five cents, 0.25
 
-    cout << "Welcome to the " << name << " apple farm" <<endl;
-    cout << "There are " <<num_apples << " apples in stock" <<endl;
-    cout << "They cost $" <<cost_each << " each." <<endl;
+    cout << "Welcome to the " <<name<< " apple farm" <<endl;
+    cout << "There are " <<num_apples<< " apples in stock" <<endl;
+    cout << "They cost $" <<cost_each<< " each." <<endl;
 
+    double total_cost=num_apples*cost_each;
+    cout << "The price for all of them is: $" << total_cost << endl;
 
     cout << endl;
     return 0;
