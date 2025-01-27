98c145ef47f4d53bb2431f70600ca94761ad0a9d
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-01-27 16:19:19 +0000

    Module 1 Tutorial 2 V.1

---
+++ Module1/m1t2_Thomas.cpp
@@ -0,0 +1,15 @@
+//CSC-134
+//Module 1 Tutorial 2
+//Andrae Thomas
+//1-27-2025
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    cout << "My name is Andrae Thomas." <<endl;
+    cout << "I am an engineering major." <<endl; 
+    cout << "A few things I like are going to the gym, playing video games, and attempting to develope my own game (a fledgling development)." <<endl;
+}
