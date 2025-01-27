aa51b6447297f858003597685950fa39b2c401d6
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-01-27 15:30:06 +0000

    Fixing hello

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello Everybody." << endl;
-    cout << "This seems to require memorization of Code strings, at least on a glance";
+    cout << "This seems to require memorization of Code strings, at least on a glance." <<endl;
+    cout << endl;
+    
 }
