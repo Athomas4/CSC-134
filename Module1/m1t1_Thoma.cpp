3aadeb72561c9e6228b904d9c2411bab11dc93cc
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-01-29 15:55:24 +0000

    M1T1 Fixes

--- Module1/m1t1_Thoma.cpp
+++ Module1/m1t1_Thoma.cpp
@@ -2,6 +2,9 @@
 //Module 1 Tutorial 2
 //Andrae Thomas
 //1-27-2025
+//Your first and last name
+//What program you're enrolled in (such as computer programming, engineering, associate of science, etc.)
+//Any additional information about yourself you'd like to volunteer -- job, hobbies, interests, etc.
 
 #include <iostream>
 
@@ -9,8 +12,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Everybody." << endl;
-    cout << "This seems to require memorization of Code strings, at least on a glance." <<endl;
+    cout << "Hello, My name is Andrae Thomas." << endl;
+    cout << "I am currently enrolled in the general engineering program as well as the civil engineering program." <<endl;
+     cout << "I like to go to the gym as well as read classic literature. My current read is wuthering heights and Don quixote." <<endl;
     cout << endl;
         return 0;// no errors
 }
