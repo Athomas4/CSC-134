30326fc43a17f81118531a6d37150cbd62067892
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-02-03 16:22:51 +0000

    Taxes_Thomas V1.01

--- Module 2/Taxes_Thomas.cpp
+++ Module 2/Taxes_Thomas.cpp
@@ -9,5 +9,24 @@ int main() {
     string food_name = "burger";
 
     cout << "Welcome to" <<store_name<< endl;
-    cout << "You have one " <<food_name<< " that is 5.99." << endl;
+    cout << "You have one " <<food_name;
+    cout << " that is $" <<food_price<< endl;
+
+    double subtotal, total;
+    subtotal = food_price;
+    cout <<"Subtotal:$" << subtotal << endl;
+
+    double tax_percent = 0.08;
+    double tax_cost = tax_percent * subtotal;
+    total = subtotal + tax_cost;
+    
+    cout << setprecision(2) << fixed;
+    cout <<endl;
+    cout <<"Your Receipt    "<< endl;
+    cout <<"-----------------"<< endl;
+    cout <<"Subtotal:   $"<<subtotal<< endl;
+    cout <<"Tax:        $"<<tax_cost<< endl;
+     cout<<"-----------------"<< endl;
+    cout <<"Total:      $"<<total<< endl;
+
 }
