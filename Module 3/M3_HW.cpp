dc9db0f7501bb8dae97e0473f5449d4b23f534cd
Athomas4 <thomasa3320@student.faytechcc.edu>
2025-03-05 15:56:53 +0000

    M3_T3-Broken?

---
+++ Module 3/M3_HW.cpp
@@ -0,0 +1,40 @@
+//CSC-134
+//Module 3 Homework
+//Andrae Thomas
+//3-2-2025
+//This File is a work in progess still
+#include <iostream>
+using namespace std;
+
+void question1();
+void question2();
+int main() {
+    cout<< "Example of Homework\n";
+    cout<<"1. Question 1\n";
+    cout<<"2. Question 2\n";
+    cout<<"0 Exit\n";
+    int choice;
+    cin >> choice;
+        if (1==choice) {
+            question1();
+        }
+        else if (2==choice){
+                question2();
+        }
+        else if (0==choice){
+                cout<< "Goodbye\n";
+        }
+        else {
+                cout<<"Not a Valid option.\n";
+        }
+    question1();
+    question2();
+    return 0; 
+}
+void question1(){
+    cout<< "Question 1 goes here\n";
+}
+void question2() {
+    int age = 30;
+    cout<<"Question 2 goes here\n"; 
+}
